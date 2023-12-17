8ac073596aa98576c2249de6995153450f8e6946
Mohamed Hamdi KAANICHE <124816027+MHKAANICHE@users.noreply.github.com>
2023-12-17 16:43:33 +0100

    Cafe Business Logic
    Learning Objectives:
    Write methods to solve basic logic problems in Java
    Practice Array and ArrayList
    Practice loops
    Understand the difference between printing and returning a value from a method
    Create your own methods file
    Write a test file and create an instance of another class to test the functionality

---
+++ CafeBusiness/CafeUtil.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.io.Console;
+import java.util.Scanner;
+
+public class CafeUtil {
+	// requirements : implement four methods.
+	
+	/*	
+	 * Cafe Java wants to implement a reward system for customers who always buy more drinks than they did the week before. To calculate how many drinks they need after 10 weeks, write a method that sums together every consecutive integer from 1 to 10 and returns the sum. In other words, add 1 + 2 + 3.. and so on up to 10 and return the result.
+	 * Test your code before moving on! Don't forget to make an instance of your CafeUtil class to use in the test file. The number printed should be 55.
+	 * 	Ninja Bonus: Add a parameter, numWeeks so that an admin can change the number from 10 to whatever they want.
+	*/
+	public int calculateStreakGoal() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+	//	Ninja Bonus:
+	public int calculateStreakGoal(int numWeeks) {
+        int sum = 0;
+        for (int i = 1; i <= numWeeks; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+	
+	/*	Given an array of item prices from an order, sum all of the prices in the array and return the total.
+	 * Don't forget to test your code! Find the lines of test code for this method in TestCafe.java and uncomment it before you compile and run.
+	 */
+	public double getOrderTotal(double[] prices) {
+		double sum = 0 ; 
+		for (double price : prices) {
+			sum += price ;
+		}
+		return sum ;
+	};
+	
+	/*
+	 * Given an ArrayList of menu items (strings), print out each index and menu item.
+	 * Sample output given an ArrayList with the items "drip coffee", "cappuccino", "latte" and "mocha"
+	 * 0 drip coffee
+	 * 1 cappuccino
+	 * 2 latte
+	 * 3 mochacopy
+	 * Re-cap of ArrayList Syntax:
+	 * String name = myArray.get(0); // to access an element in an ArrayList using an index
+	 */
+	public void displayMenu(ArrayList<String> menuItems) {
+		for (int count = 0; count < menuItems.size(); count++) {
+			String message = count + " " + menuItems.get(count);
+			System.out.println(message);
+		}
+		
+	};
+	
+	
+	/*
+	 * Inside this method:
+	 * Print this string to the console: "Please enter your name:"
+	 * Then add this line of code to get user input from the terminal and store it in the variable userName:
+	 * String userName = System.console().readLine();
+	 * Next print the userName to the console, saying "Hello, [user name here]!"
+	 * Next print "There are ___ people in front of you" using the number for how many people are ahead of them (how many items already in the array)
+	 * Then, add the customer's name to the given customers list. And print the list.
+	 * No need to return anything.
+	 * myArray.add("Heidi"); // to add an item to an ArrayList
+	 */
+	public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name:");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        printGreetingAndQueuePosition(userName, customers.size());
+
+        customers.add(userName);
+
+        printCustomerList(customers);
+
+        //scanner.close(); // Close the scanner to prevent resource leak
+    }
+
+    private void printGreetingAndQueuePosition(String userName, int queueSize) {
+        System.out.printf("Hello, %s%n", userName);
+        System.out.printf("There are %d people in front of you%n", queueSize);
+    }
+
+    private void printCustomerList(ArrayList<String> customers) {
+        System.out.println("Order ------  Customer Name%");
+        for (int count = 0; count < customers.size(); count++) {
+            System.out.printf("%d 	------  %s%n", count, customers.get(count));
+        }
+        System.out.println("------------------------");
+    }
+}
