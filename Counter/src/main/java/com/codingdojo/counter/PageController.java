502cbe723480205cf53fcebf48871486f8d0eecf
Mohamed Hamdi KAANICHE <124816027+MHKAANICHE@users.noreply.github.com>
2023-12-19 00:19:25 +0100

    Counter
    Practice using session to store data about a particular client's history with the app
    Be able to check whether a session variable exists
    Use dependency injection to initialize session
    Use getAttribute and setAttribute to read and modify session

--- Counter/src/main/java/com/codingdojo/counter/PageController.java
+++ Counter/src/main/java/com/codingdojo/counter/PageController.java
@@ -2,29 +2,35 @@ package com.codingdojo.counter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import jakarta.servlet.http.HttpSession;
 @Controller
-@RequestMapping("/")
 public class PageController {
 	@RequestMapping("/")
 	public String index(HttpSession session, Model model) {
 		// Session Control :
-		checkSession(session);
-	
+		checkSession(session,1);
 		// Model routing control :
-		model.addAttribute("page","your_server");
-	
+		model.addAttribute("page","your_server");	
 		return "index.jsp";
 	}
 	@RequestMapping("/counter")
 	public String counter(HttpSession session, Model model) {
-		
+		checkSession(session,1);
 		// Model routing control :
 		model.addAttribute("page","counter");
 		return "index.jsp";
 	}
 	
-	private void checkSession(HttpSession session) {
+	@RequestMapping("/Extracounter")
+	public String extraCounter(HttpSession session, Model model) {
+		checkSession(session,2);
+		// Model routing control :
+		model.addAttribute("page","counter");
+		return "index.jsp";
+	}
+	
+	private void checkSession(HttpSession session, Integer var) {
 		/*
 		 * getAttribute(String name) Returns the object bound with the specified name in
 		 * this session, or null if no object is bound under the name.
@@ -33,7 +39,7 @@ public class PageController {
 			session.setAttribute("count", 0);
 		} else {
 			int count = (int) session.getAttribute("count");
-			count += 1;
+			count += var;
 			session.setAttribute("count", count);
 		}
 	}
